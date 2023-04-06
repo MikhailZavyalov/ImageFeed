0552d6540ecd713caffcba800e07f399d7eaba6f
Mikhail Zavyalov <mikhailzavyalov0807@gmail.com>
2023-04-06 19:00:53 +0400

    SwiftKeychainWrapper added

--- ImageFeed/ServiceAccessLayer/OAuth2/OAuth2TokenStorage.swift
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2TokenStorage.swift
@@ -1,14 +1,37 @@
 
 import Foundation
+import SwiftKeychainWrapper
 
 final class OAuth2TokenStorage {
-   static private let bearerTokenKey = "imageFeedBearerToken"
-   static var token: String? {
+    static private let bearerTokenKey = "imageFeedBearerToken"
+    static var token: String? {
         get {
-            UserDefaults.standard.string(forKey: bearerTokenKey)
+            let token: String? = KeychainWrapper.standard.string(forKey: OAuth2TokenStorage.bearerTokenKey)
+            return token
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: bearerTokenKey)
+            let isSuccess = KeychainWrapper.standard.set(newValue!, forKey: OAuth2TokenStorage.bearerTokenKey)
         }
     }
+    
+    
+    //    var isSuccess: String? {
+    //        get {
+    //            let getToken: String? = KeychainWrapper.standard.string(forKey: OAuth2TokenStorage.bearerTokenKey)
+    //            return getToken
+    //        }
+    //        set {
+    //            let setToken: String? = KeychainWrapper.standard.set(token, forKey: OAuth2TokenStorage.bearerTokenKey)
+    //        }
+    //    }
+    
+    
+    //       static var token: String? {
+    //            get {
+    //                UserDefaults.standard.string(forKey: bearerTokenKey)
+    //            }
+    //            set {
+    //                UserDefaults.standard.set(newValue, forKey: bearerTokenKey)
+    //            }
+    //        }
 }
